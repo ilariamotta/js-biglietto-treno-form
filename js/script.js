63186751f369cb489e4bb89d028f03034ecaede2
ilariamotta <ilaria.motta93@gmail.com>
2025-11-06 16:19:36 +0100

    feat: inserito parametro età

--- js/script.js
+++ js/script.js
@@ -5,53 +5,58 @@ const datiInput = document.getElementById("anagrafica");
 // console.log("datiInput");
 const kmInput = document.getElementById("percorrenza");
 // console.log("kmInput");
+const etaInput = document.getElementById("fasciaeta");
+// console.log("etaInput");
 
 
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", function (event) {
     event.preventDefault();
     const datiUtente = datiInput.value;
     const kmViaggio = kmInput.value;
+    const etaUtente = etaInput.value;
+    console.log(etaUtente);
     const kmBase = 0.21
     const kmPrezzo = parseInt(kmViaggio) * kmBase;
     let sconto = "";
-if (etaUtente <= 18) {
-    sconto = 0.2;
-} else if (etaUtente >= 65) {
-    sconto = 0.4;
-}
-const prezzoFinale = kmPrezzo * (1 - sconto);
-const risultato = `Il prezzo del biglietto è di ${prezzoFinale.toFixed(2)}€`;
-console.log(kmBase);
+
+    if (etaUtente <= 18) {
+        sconto = 0.2;
+    } else if (etaUtente >= 65) {
+        sconto = 0.4;
+    }
+    const prezzoFinale = kmPrezzo * (1 - sconto);
+    const risultato = `Il prezzo del biglietto è di ${prezzoFinale.toFixed(2)}€`;
+    console.log(risultato);
 });
 
 
 
 
 
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
