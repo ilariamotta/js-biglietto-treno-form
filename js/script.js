e393c057d146351cc70834b9973967255e5a4381
ilariamotta <ilaria.motta93@gmail.com>
2025-11-06 15:53:28 +0100

    feat: elementi dom

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+const form = document.querySelector("form");
+// console.log("form");
+
+const datiUtente = document.getElementById("anagrafica");
+// console.log("datiUtente");
+const kmViaggio = document.getElementById("percorrenza");
+// console.log("kmViaggio");
+const genButton = document.getElementById("genera");
+// console.log("genButton");
+
+
+
+
 
    
    
