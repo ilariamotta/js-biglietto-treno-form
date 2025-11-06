3c60d7f317a1a798bba32fef11d178bbfe134575
ilariamotta <ilaria.motta93@gmail.com>
2025-11-06 18:08:34 +0100

    feat: stile pagina

--- js/script.js
+++ js/script.js
@@ -31,31 +31,31 @@ form.addEventListener("submit", function (event) {
     const risultato = `${prezzoFinale.toFixed(2)}€`;
     console.log(risultato);
 
-    section.innerHTML = `<div class="row d-flex justify-content-center">
-                <h2>IL TUO BIGLIETTO</h2>
-                <div class="col-4">
-                    <h3>Dettaglio passeggeri</h3>
-                    <h4>NOME PASSEGGERO</h4>
-                    <h5>Nome passeggero</h5>
-                    <span class="nome">${datiUtente}</span>
-                </div>
-                <div class="col-2">
-                    <h5>Offerta</h5>
-                    <span class="tipoBigl">${etaUtente}</span>
-                </div>
-                <div class="col-2">
-                <h5>Carrozza</h5>
-                <span class="nCarrozza">${numCar}</span>
-                </div>
-                <div class="col-2">
-                <h5>Codice CP</h5>
-                <span class="codCp">${numCp}</span>
-                </div>
-                <div class="col-2">
-                <h5>Costo Biglietto</h5>
-                <span class="costoBigl">${prezzoFinale.toFixed(2)}€</span>
-                </div>
-            </div>`
+    section.innerHTML = `<div class="row d-flex justify-content-center bg-light rounded">
+                    <h2 class="bg-success rounded py-2">IL TUO BIGLIETTO</h2>
+                    <div class="col-4">
+
+                        <h4>NOME PASSEGGERO</h4>
+                        <span class="nome">${datiUtente}</span>
+                    </div>
+                    <div class="col-2">
+                        <h5>Offerta</h5>
+                        <span class="tipoBigl">${etaUtente}</span>
+                    </div>
+                    <div class="col-2">
+                        <h5>Carrozza</h5>
+                        <span class="nCarrozza">${numCar}</span>
+                    </div>
+                    <div class="col-2">
+                        <h5>Codice CP</h5>
+                        <span class="codCp">${numCp}</span>
+                    </div>
+                    <div class="col-2">
+                        <h5>Costo Biglietto</h5>
+                        <span class="costoBigl">${risultato}</span>
+                    </div>
+                    <div class="col-12 bg-danger rounded py-1 display-6"><strong>Buon Viaggio!</strong></div>
+                </div>`
 section.classList.remove("d-none");
 });
 
